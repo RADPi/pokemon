150053ebac4fa34cdabe5e0dae83319906ef9f81
Rafael Dávila Pico <59027415+RADPi@users.noreply.github.com>
2022-05-05 14:38:59 -0300

    Formik

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,42 +1,37 @@
 import React from 'react'
 import { Formik, Form, Field, ErrorMessage } from 'formik'
+import useAuth from '../hooks/useAuth'
+import { actionLogin } from '../auth/authReducer'
 
 const Login = () => {
+	const { authState, dispatch } = useAuth()
+
+	const handleSubmit = values => {
+		actionLogin(values, dispatch)
+	}
 	return (
 		<div className='container'>
-			<h1>Login</h1>
-			<Formik
-				initialValues={{ email: '', password: '' }}
-				validate={values => {
-					const errors = {}
-					if (!values.email) {
-						errors.email = 'Required'
-					} else if (
-						!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-					) {
-						errors.email = 'Invalid email address'
-					}
-					return errors
-				}}
-				onSubmit={(values, { setSubmitting }) => {
-					setTimeout(() => {
-						alert(JSON.stringify(values, null, 2))
-						setSubmitting(false)
-					}, 400)
-				}}
-			>
-				{({ isSubmitting }) => (
-					<Form>
-						<Field type='email' name='email' />
-						<ErrorMessage name='email' component='div' />
-						<Field type='password' name='password' />
-						<ErrorMessage name='password' component='div' />
-						<button type='submit' disabled={isSubmitting}>
-							Submit
-						</button>
-					</Form>
-				)}
-			</Formik>
+			<div className='card col-md-6 mx-auto mt-4 p-4'>
+				<h1>Login</h1>
+				<Formik
+					initialValues={{ name: '' }}
+					onSubmit={values => handleSubmit(values)}
+				>
+					{() => (
+						<Form>
+							<div className='form-group mb-4'>
+								<label className='form-label'>Ingrese su nombre</label>
+								<Field className='form-control' type='text' name='name' />
+							</div>
+							<div className='form-group'>
+								<button className='btn btn-primary' type='submit'>
+									Ingresar
+								</button>
+							</div>
+						</Form>
+					)}
+				</Formik>
+			</div>
 		</div>
 	)
 }
